58ecd49958441807590f171403ee6c51e89fd63f
ABHIGYAN KUMAR GUPTA <abhigyankumar268@gmail.com>
2025-09-18 22:08:51 +0530

    route js updated

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,13 +1,13 @@
 import { serve } from "inngest/next";
-import { inngest } from "@/components/inngest/client";
-import { syncUserCreation, syncUserDeletion, synUserUpdation } from "@/components/inngest/functions";
+import { inngest } from "../../../inngest/client";
+import { syncUserCreation, syncUserDeletion, synUserUpdation } from "@/inngest/functions";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    syncUserCreation,
-    synUserUpdation,
-    syncUserDeletion
+   syncUserCreation,
+   synUserUpdation,
+   syncUserDeletion
   ],
 });
